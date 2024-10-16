feeacc86efa8676d759e0fab364b2f9f2cced5b9
emilh7279 <emilh7279@gmail.com>
2024-10-16 22:14:51 +0200

    Update Comments in simple_textblob.py

--- textblob-de/simple_textblob.py
+++ textblob-de/simple_textblob.py
@@ -1,11 +1,20 @@
 from textblob_de import TextBlobDE as TextBlob
+
+"""
+If you’re unsure of which datasets/models you’ll need, you can install the “popular” subset of NLTK data,
+on the command line type python -m nltk.downloader popular, or in the Python interpreter import nltk; nltk.download('popular')
+
+NLTK Toolkit Natural Language Toolkit muss vor dem beim ersten Ausführen des Codes auskommentiert werden.
+"""
+
 #import nltk
+#nltk.download('popular')
 #nltk.download('punkt')
 
 def satz_bewertung(satz):
-    """
-    Bewertet die Stimmung eines deutschen Satzes (positiv, negativ oder neutral).
-    """
+
+    # Bewertet die Stimmung eines deutschen Satzes (positiv, negativ oder neutral).
+
     # Erstellen eines TextBlob-Objekts
     blob = TextBlob(satz)
 
@@ -23,11 +32,16 @@ def satz_bewertung(satz):
 
 # Testen mit einigen Beispielsätzen
 sätze = [
-    "Ich liebe dieses Wetter.",
-    "Das Essen schmeckt schrecklich!",
-    "Heute ist ein ganz normaler Tag.",
-    "Die Frau arbeitet langsam!",
-    "Die Frau kocht fantastisch"
+    "Der Service war hervorragend und mein Anliegen wurde sofort geklärt.",
+    "Der Prozess war etwas langwierig, aber das Endergebnis war in Ordnung.",
+    "Der Mitarbeiter war sehr unhöflich und mein Problem wurde nicht behoben.",
+    "Schlechter Kundenservice, niemand hat auf meine Anfragen geantwortet.",
+    "Der Service hat zu lange gebraucht und mein Problem nicht gelöst.",
+    "Ich bin wirklich beeindruckt, wie schnell mein Problem gelöst wurde.",
+    "Fantastischer Service! Mein Problem wurde sofort gelöst.",
+    "Es hat viel zu lange gedauert und niemand konnte mir helfen.",
+    "Es war okay, nicht das Beste, aber das Problem wurde gelöst.",
+    "Das Support-Team schien verwirrt und hat nicht wirklich geholfen."
 ]
 
 for satz in sätze:
